3812fecb6abe5434e8f7336514bf4176af8d19e7
zeusilein <73743279+zeusilein@users.noreply.github.com>
2025-07-22 07:23:42 +0200

    Update scan.sh
    remove duplex, as already performed before

--- scan.sh
+++ scan.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 now=`date +"%Y-%m-%d-%H%M%S"`
-/app/sane-scan-pdf/scan -d -r 200 -v --mode 'Gray' --duplex --crop --ocr --skip-empty-pages -o /scans/scan-$now.pdf
+/app/sane-scan-pdf/scan -d -r 200 -v --mode 'Gray' --crop --ocr --skip-empty-pages -o /scans/scan-$now.pdf
