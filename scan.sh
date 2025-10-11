df4c17d8614a4ab6027d5aed233b5795a363c862
zeusilein <73743279+zeusilein@users.noreply.github.com>
2025-10-11 12:29:35 +0200

    Delete scan.sh
    content is included in dockerfile

--- scan.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-now=`date +"%Y-%m-%d-%H%M%S"`
-/app/sane-scan-pdf/scan -d -r $SCAN_RESOLUTION -v --mode "$SCAN_MODE" --crop --ocr --skip-empty-pages -o /scans/scan-$now.pdf
-/usr/bin/gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=/scans/scan-${now}-ebook.pdf /scans/scan-${now}.pdf
